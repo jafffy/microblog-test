61f2f516e2c8fc522bd0dc9be9b9963d9673d5eb
cocos2d-x-dev <cocos2d-x-dev@pilsu-iui-iMac.local>
2014-12-29 14:28:43 +0900

    virtualenv는 path specific하지 않게 세팅할 때까지는 일단 ignore 하기로 한다

--- flask/bin/activate.fish
+++ flask/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/var/www/flask"
+set -gx VIRTUAL_ENV "/Users/cocos2d-x-dev/microblog-test/flask"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
