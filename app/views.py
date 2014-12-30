28267fd883850eeaefd57c5860d106e7c79196df
Jaewon Choi <jaiersoer@ajou.ac.kr>
2014-12-30 12:47:08 +0900

    login view 만들어 봄

--- app/views.py
+++ app/views.py
@@ -27,6 +27,8 @@ def login():
     if form.validate_on_submit():
         flash('Login requested for OpenID="%s", remember_me=%s' %
             (form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
     return render_template('login.html',
                             title='Sign In',
-                            form=form)
+                            form=form,
+                            providers=app.config['OPENID_PROVIDERS'])
--- config.py
+++ config.py
@@ -1,2 +1,6 @@
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'beep62656570'
+
+OPENID_PROVIDERS = [
+    {'name': 'Google', 'url': 'https://www.google.com/acounts/o8/id'}
+]
